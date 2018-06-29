34f49d25e5e9d6de761470e31fe9118ef74f3082
Joe Shepherd <joeshepmedia@gmail.com>
2018-06-28 21:40:44 -0500

    Refactor API call to return my 'owned games' collection; display in DOM

--- bgg_interface/user_collection/services.py
+++ bgg_interface/user_collection/services.py
@@ -6,18 +6,24 @@ import xmltodict
 class ApiService():
   # Do we have to use the decorator for static and class methods?
   @staticmethod
-  def get_collection():
+  def get_collection(username="lucegoose100"):
     result = {}
     response = requests.get(
-        "https://www.boardgamegeek.com/xmlapi2/thing?id=220877")
+        "https://www.boardgamegeek.com/xmlapi2/collection?username=" + username + "&own=1")
     if response.status_code == 200:
       # Search the XML results for the 'items' element
       converted_data = xmltodict.parse(response.content)
       # NOW we use the xmltodict module to deserialize the json to a Python dictionary
-      result['title'] = converted_data['items']['item']['name'][0]['@value']
+      result['list'] = converted_data['items']['item']
       # And we can add a property to it, since it's friendly old python now
       result["success"] = True
+      for game in result["list"]:
+        game["name"]["text"] = game["name"]["#text"]
+      return result
     else:
       print("HTTP FAILED", response.status_code)
 
     return result
+
+
+
